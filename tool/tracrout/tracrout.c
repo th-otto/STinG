e096207b164e38c672453fdbd1e6f11ec5cba288
Thorsten Otto <admin@tho-otto.de>
2021-05-18 16:50:51 +0200

    traceroute: move strings to resource files

--- tool/tracrout/tracrout.c
+++ tool/tracrout/tracrout.c
@@ -16,6 +16,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "transprt.h"
 #include "layer.h"
@@ -68,16 +69,23 @@ static uint16 hop_c;
 static uint16 hop_d;
 static uint32 host;
 static char alert[200];
-static char const not_there[] = "[1][ |  STinG is not loaded or enabled !   ][ Hmmm ]";
-static char const corrupted[] = "[1][ |  STinG structures corrupted !   ][ Oooops ]";
-static char const found_it[] = "[3][ |  Driver \'%s\',|  by %s, found,   |  version %s.][ Okay ]";
-static char const no_module[] = "[1][ |  STinG Transport Driver not found !   ][ Grmbl ]";
-static char const format[] = "[0][ |  Hop #%u :| |     Address %u.%u.%u.%u   ][ Okay ]";
-static char const timeout[] = "[1][ |  Timeout awaiting   | |    response  !][ Retry | Cancel ]";
-static char const no_route[] = "[1][ |   No Route !   ][ Hmmm ]";
-static char const no_memory[] = "[1][ |   Out of internal memory !   ][ Ooops ]";
-static char const no_handler[] = "[1][ | Cannot set ICMP handler ][ Grmbl ]";
-static char const no_stik[] = "[1][ | Don't use this Program | |  with STiK !][ Abort ]";
+
+
+static char *rs_frstr(_WORD num)
+{
+	char *str = NULL;
+	rsrc_gaddr(R_STRING, num, &str);
+	return str;
+}
+
+
+static OBJECT *rs_tree(_WORD num)
+{
+	OBJECT *tree = NULL;
+	rsrc_gaddr(R_TREE, num, &tree);
+	return tree;
+}
+
 
 
 
@@ -113,11 +121,126 @@ static int16 cdecl receive_echo(IP_DGRAM *datagram)
 }
 
 
-static void fetch_parameters(void)
+static void set_str(OBJECT *tree, _WORD obj, const char *str)
+{
+	TEDINFO *ted = tree[obj].ob_spec.tedinfo;
+	strncpy(ted->te_ptext, str, ted->te_txtlen - 1);
+}
+
+
+/*
+ * Check whether "cp" is a valid ascii representation
+ * of an Internet address and convert to a binary address.
+ * Returns 1 if the address is valid, 0 if not.
+ * This replaces inet_addr, the return value from which
+ * cannot distinguish between failure and a local broadcast address.
+ */
+static int inet_aton(const char *cp, uint32 *addr)
+{
+	uint32 val;
+	int base;
+	int n;
+	unsigned char c;
+	unsigned long parts[4];
+	unsigned long *pp = parts;
+
+	c = *cp;
+	for (;;)
+	{
+		/*
+		 * Collect number up to ``.''.
+		 * Values are specified as for C:
+		 * 0x=hex, 0=octal, isdigit=decimal.
+		 */
+		if (!isdigit(c))
+			return 0;
+		base = 10;
+		if (c == '0')
+		{
+			c = *++cp;
+			if (c == 'x' || c == 'X')
+				base = 16, c = *++cp;
+			else
+				base = 8;
+		}
+		val = 0;
+		for (;;)
+		{
+			if (isascii(c) && isdigit(c))
+			{
+				val = (val * base) + (c - '0');
+				c = *++cp;
+			} else if (base == 16 && isascii(c) && isxdigit(c))
+			{
+				val = (val << 4) |
+					(c + 10 - (islower(c) ? 'a' : 'A'));
+				c = *++cp;
+			} else
+				break;
+		}
+		if (c == '.')
+		{
+			/*
+			 * Internet format:
+			 *	a.b.c.d
+			 *	a.b.c	(with c treated as 16 bits)
+			 *	a.b	(with b treated as 24 bits)
+			 */
+			if (pp >= parts + 3)
+				return 0;
+			*pp++ = val;
+			c = *++cp;
+		} else
+			break;
+	}
+	/*
+	 * Check for trailing characters.
+	 */
+	if (c != '\0' && (!isascii(c) || !isspace(c)))
+		return 0;
+	/*
+	 * Concoct the address according to
+	 * the number of parts specified.
+	 */
+	n = (int)(pp - parts + 1);
+	switch (n)
+	{
+	case 0:
+		return 0;		/* initial nondigit */
+
+	case 1:				/* a -- 32 bits */
+		break;
+
+	case 2:				/* a.b -- 8.24 bits */
+		if (parts[0] > 0xff || val > 0xffffffUL)
+			return 0;
+		val |= parts[0] << 24;
+		break;
+
+	case 3:				/* a.b.c -- 8.8.16 bits */
+		if (parts[0] > 0xff || parts[1] > 0xff || val > 0xffffUL)
+			return 0;
+		val |= (parts[0] << 24) | (parts[1] << 16);
+		break;
+
+	case 4:				/* a.b.c.d -- 8.8.8.8 bits */
+		if (parts[0] > 0xff || parts[1] > 0xff || parts[2] > 0xff || val > 0xff)
+			return 0;
+		val |= (parts[0] << 24) | (parts[1] << 16) | (parts[2] << 8);
+		break;
+	}
+	if (addr)
+		*addr = htonl(val);
+
+	return 1;
+}
+
+
+static int fetch_parameters(void)
 {
 	OBJECT *tree;
-	char *txt;
-	_WORD x, y, w, h, c_x, c_y;
+	GRECT gr;
+	_WORD button;
 
 	graf_mouse(ARROW, NULL);
 
@@ -125,34 +248,28 @@ static void fetch_parameters(void)
 
 	wind_update(BEG_UPDATE);
 
-	form_center(tree, &x, &y, &w, &h);
-	c_x = x + w / 2;
-	c_y = y + h / 2;
-	form_dial(FMD_START, c_x, c_y, 0, 0, x, y, w, h);
-	form_dial(FMD_GROW, c_x, c_y, 0, 0, x, y, w, h);
+	form_center_grect(tree, &gr);
+	form_dial_grect(FMD_START, &gr, &gr);
 
-	*tree[HOST].ob_spec.tedinfo->te_ptext = '\0';
-	strcpy(tree[MAX_TTL].ob_spec.tedinfo->te_ptext, "30");
+	set_str(tree, MODULE, tpl->module);
+	set_str(tree, AUTHOR, tpl->author);
+	set_str(tree, VERSION, tpl->version);
+	set_str(tree, HOST, "");
+	set_str(tree, MAX_TTL, "30");
 
-	objc_draw(tree, ROOT, MAX_DEPTH, x, y, w, h);
-	form_do(tree, HOST);
+	objc_draw_grect(tree, ROOT, MAX_DEPTH, &gr);
+	button = form_do(tree, HOST);
 
-	form_dial(FMD_SHRINK, c_x, c_y, 0, 0, x, y, w, h);
-	form_dial(FMD_FINISH, c_x, c_y, 0, 0, x, y, w, h);
+	form_dial_grect(FMD_FINISH, &gr, &gr);
 
 	wind_update(END_UPDATE);
 
+	if (button != OK)
+		return 0;
 	max_ttl = atoi(tree[MAX_TTL].ob_spec.tedinfo->te_ptext);
-	txt = tree[HOST].ob_spec.tedinfo->te_ptext;
-	txt[12] = '\0';
-	h = atoi(&txt[9]);
-	txt[9] = '\0';
-	w = atoi(&txt[6]);
-	txt[6] = '\0';
-	y = atoi(&txt[3]);
-	txt[3] = '\0';
-	x = atoi(&txt[0]);
-	host = ((uint32) x << 24) | ((uint32) y << 16) | ((uint32) w << 8) | (uint32) h;
+	if (inet_aton(tree[HOST].ob_spec.tedinfo->te_ptext, &host) == 0)
+		return 0;
+	return 1;
 }
 
 
@@ -162,6 +279,7 @@ static _WORD do_alert(const char *str)
 	_WORD message[8];
 	_WORD dummy;
 	
+	graf_mouse(ARROW, NULL);
 	ret = form_alert(1, str);
 	/*
 	 * let other windows redraw in multitask-AES
@@ -186,16 +304,15 @@ static void do_some_work(void)
 	int16 what;
 	uint32 chksum = 0;
 
-	fetch_parameters();
-
-	if (host == 0)
+	if (fetch_parameters() == 0)
 		return;
 
 	if (PRTCL_get_parameters(host, &packet.ps_hdr.src_ip, NULL, NULL) != E_NORMAL)
 	{
-		do_alert(no_route);
+		do_alert(rs_frstr(NO_ROUTE));
 		return;
 	}
+
 	packet.ps_hdr.dest_ip = host;
 	packet.ps_hdr.zero = 0;
 	packet.ps_hdr.protocol = P_UDP;
@@ -213,17 +330,17 @@ static void do_some_work(void)
 
 	if (!ICMP_handler(receive_echo, HNDLR_SET))
 	{
-		do_alert(no_handler);
+		do_alert(rs_frstr(NO_HANDLER));
 		return;
 	}
 
 	for (count = 1; count <= max_ttl; count++)
 	{
+		graf_mouse(BUSY_BEE, NULL);
 		if ((dgram = KRmalloc(sizeof(UDP_PCKT))) == NULL)
 		{
-			do_alert(no_memory);
-			ICMP_handler(receive_echo, HNDLR_REMOVE);
-			return;
+			do_alert(rs_frstr(NO_MEMORY));
+			break;
 		}
 		memcpy(dgram, &packet.udp_packet, sizeof(UDP_PCKT));
 
@@ -234,10 +351,9 @@ static void do_some_work(void)
 
 		if (what == E_NOMEM || what == E_UNREACHABLE)
 		{
-			do_alert("[1][ |   Problem sending packet !   ][ Hmmm ]");
+			do_alert(rs_frstr(PROB_SEND_PACKET));
 			KRfree(dgram);
-			ICMP_handler(receive_echo, HNDLR_REMOVE);
-			return;
+			break;
 		}
 
 		for (wait = 0; wait < 200; wait++)
@@ -250,25 +366,27 @@ static void do_some_work(void)
 		switch (response)
 		{
 		case 0:
-			if (do_alert(timeout) == 1)
+			if (do_alert(rs_frstr(TIMEOUT)) == 1)
 			{
 				count--;
 				break;
 			}
-			ICMP_handler(receive_echo, HNDLR_REMOVE);
-			return;
+			count = max_ttl;
+			break;
 		case ICMP_TIME_EXCEED:
-			sprintf(alert, format, count, hop_a, hop_b, hop_c, hop_d);
+			sprintf(alert, rs_frstr(HOP_RESULT), count, hop_a, hop_b, hop_c, hop_d);
 			do_alert(alert);
 			break;
 		case ICMP_DEST_UNREACH:
-			do_alert(code == ICMP_DU_PRTCL || code == ICMP_DU_PORT ?
-					   "[3][ |   We've reached| |     the destination !  ][ Great ]" :
-					   "[1][ |   We can't get to| |     the destination !  ][ Pity ]");
-			ICMP_handler(receive_echo, HNDLR_REMOVE);
-			return;
+			do_alert(rs_frstr(code == ICMP_DU_PRTCL || code == ICMP_DU_PORT ? DEST_REACHED : NO_DEST));
+			count = max_ttl;
+			break;
 		}
 	}
+
+	ICMP_handler(receive_echo, HNDLR_REMOVE);
+
+	graf_mouse(ARROW, NULL);
 }
 
 
@@ -293,12 +411,12 @@ static void gem_program(void)
 
 	if (sting_drivers == 0L)
 	{
-		do_alert(not_there);
+		do_alert(rs_frstr(NOT_THERE));
 		return;
 	}
 	if (strcmp(sting_drivers->magic, STIK_DRVR_MAGIC) != 0)
 	{
-		do_alert(corrupted);
+		do_alert(rs_frstr(CORRUPTED));
 		return;
 	}
 
@@ -309,16 +427,14 @@ static void gem_program(void)
 	{
 		if (stx == NULL)
 		{
-			do_alert(no_stik);
+			do_alert(rs_frstr(NO_STIK));
 		} else
 		{
-			sprintf(alert, found_it, tpl->module, tpl->author, tpl->version);
-			do_alert(alert);
 			do_some_work();
 		}
 	} else
 	{
-		do_alert(no_module);
+		do_alert(rs_frstr(NO_MODULE));
 	}
 }
 
--- tool/tracrout/tracrout.h
+++ tool/tracrout/tracrout.h
@@ -1,23 +1,23 @@
 /*
  * resource set indices for tracrout
  *
- * created by ORCS 2.16
+ * created by ORCS 2.18
  */
 
 /*
- * Number of Strings:        8
+ * Number of Strings:        33
  * Number of Bitblks:        0
  * Number of Iconblks:       0
  * Number of Color Iconblks: 0
  * Number of Color Icons:    0
- * Number of Tedinfos:       2
- * Number of Free Strings:   0
+ * Number of Tedinfos:       5
+ * Number of Free Strings:   12
  * Number of Free Images:    0
- * Number of Objects:        5
+ * Number of Objects:        12
  * Number of Trees:          1
  * Number of Userblks:       0
  * Number of Images:         0
- * Total file size:          320
+ * Total file size:          1368
  */
 
 #undef RSC_NAME
@@ -35,24 +35,53 @@
 # define RSC_STATIC_FILE 0
 #endif
 #if !RSC_STATIC_FILE
-#define NUM_STRINGS 8
-#define NUM_FRSTR 0
+#define NUM_STRINGS 33
+#define NUM_FRSTR 12
 #define NUM_UD 0
 #define NUM_IMAGES 0
 #define NUM_BB 0
 #define NUM_FRIMG 0
 #define NUM_IB 0
 #define NUM_CIB 0
-#define NUM_TI 2
-#define NUM_OBS 5
+#define NUM_TI 5
+#define NUM_OBS 12
 #define NUM_TREE 1
 #endif
 
 
 
-#define TRACROUT   0 /* form/dialog */
-#define HOST       2 /* FTEXT in tree TRACROUT */ /* max len 12 */
-#define MAX_TTL    3 /* FTEXT in tree TRACROUT */ /* max len 3 */
+#define TRACROUT                           0 /* form/dialog */
+#define MODULE                             2 /* TEXT in tree TRACROUT */
+#define AUTHOR                             4 /* TEXT in tree TRACROUT */
+#define VERSION                            6 /* TEXT in tree TRACROUT */
+#define HOST                               8 /* FTEXT in tree TRACROUT */
+#define MAX_TTL                            9 /* FTEXT in tree TRACROUT */
+#define OK                                10 /* BUTTON in tree TRACROUT */
+#define CANCEL                            11 /* BUTTON in tree TRACROUT */
+
+#define NOT_THERE                          0 /* Alert string */
+
+#define CORRUPTED                          1 /* Alert string */
+
+#define NO_MODULE                          2 /* Alert string */
+
+#define TIMEOUT                            3 /* Free string */
+
+#define NO_ROUTE                           4 /* Alert string */
+
+#define NO_MEMORY                          5 /* Alert string */
+
+#define NO_HANDLER                         6 /* Alert string */
+
+#define NO_STIK                            7 /* Alert string */
+
+#define PROB_SEND_PACKET                   8 /* Alert string */
+
+#define HOP_RESULT                         9 /* Alert string */
+
+#define DEST_REACHED                      10 /* Alert string */
+
+#define NO_DEST                           11 /* Alert string */
 
 
 
